ada69e90ddf8a2528dc569e84b87b910a66253c8
aulia <aulia22trk@mahasiswa.pcr.ac.id>
2024-06-26 00:15:43 +0700

    commit sebelum navbar

--- app/src/main/java/com/aulicious/gvood/SplashActivity.java
+++ app/src/main/java/com/aulicious/gvood/SplashActivity.java
@@ -1,2 +1,34 @@
-package com.aulicious.gvood;public class SplashActivity {
+package com.aulicious.gvood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        // inisialisasi view
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        // Hide ActionBar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+        @Override
+        public void run () {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }
+        },3000L);
+    }
 }
